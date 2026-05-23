3f1e5415baa008927e14cb2e46118c6ff9341e62
Akintade Oluwaseun <akintadeseun816@gmail.com>
2026-05-23 17:21:41 +0100

    Initial Commit

---
+++ PizzaTracker/PizzaTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaTrackerApp.swift
+//  PizzaTracker
+//
+//  Created by BSTAR on 23/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct PizzaTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
